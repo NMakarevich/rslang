135d1c01be48194b212c3c61d14e7d5fabd3dd3b
Nikolai Makarevich <nikoladze529@mail.ru>
2022-02-03 21:53:42 +0300

    feat: add loading main page

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,10 @@
 import './sass/style.scss';
+import Main from './components/main';
+import Router from './components/router';
+
+window.addEventListener('load', () => {
+  const main = new Main();
+  document.body.innerHTML = main.render();
+  main.eventListener();
+  new Router();
+});
