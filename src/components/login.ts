6f259e477e4429a07b942a629da4634b42081488
Nikolai Makarevich <nikoladze529@mail.ru>
2022-02-05 16:00:56 +0300

    feat: update render content

--- src/components/login.ts
+++ src/components/login.ts
@@ -1,10 +1,42 @@
-const login = {
-  render: (): string => {
+class Login {
+  email: HTMLInputElement;
+  password: HTMLInputElement;
+  submit: HTMLInputElement;
+
+  constructor() {
+    this.email = document.createElement('input');
+    this.password = document.createElement('input');
+    this.submit = document.createElement('input');
+  }
+  render = (): string => {
     return `
     <div class='main__login'>
       <h2 сlass='main__title'>Вход</h2>
+      <form class='main__form'>
+        ${this.insertInputs()}
+      </form>
+      <a class='registry__link' href='#/registry'>Регистрация</a>
     </div>`;
-  },
+  }
+
+  insertInputs(): string {
+    this.email.type = 'email';
+    this.email.id = 'email';
+    this.email.placeholder = 'Email';
+    this.password.type = 'password';
+    this.password.id = 'password';
+    this.password.placeholder = 'Пароль';
+    this.submit.type = 'submit';
+    this.submit.value = 'Войти';
+
+    const div = document.createElement('div');
+    div.append(this.email, this.password, this.submit);
+    return div.innerHTML;
+  }
+
+  login = async () => {
+    // await loginUser(this.email.value, this.password.value);
+  }
 };
 
-export default login;
+export default Login;
--- src/components/registry.ts
+++ src/components/registry.ts
@@ -1,11 +1,36 @@
-const registry = {
-  render: (): string => {
+import Login from "./login";
+
+class Registry extends Login {
+  name: HTMLInputElement;
+  constructor() {
+    super();
+    this.name = document.createElement('input');
+  }
+  override render = (): string => {
     return `
     <div class='main__registry'>
       <h2 сlass='main__title'>Регистрация</h2>
+      <form class='main__form'>
+        ${this.insertInputs()}
+      </form>
     </div>
     `;
-  },
+  }
+  override insertInputs(): string {
+    super.insertInputs();
+    this.name.type = 'text';
+    this.name.id = 'name';
+    this.name.placeholder = 'Имя пользователя';
+    this.submit.value = 'Зарегистрироваться'
+    
+    const div = document.createElement('div');
+    div.append(this.name, this.email, this.password, this.submit);
+    return div.innerHTML;    
+  }
+
+  registry = async () => {
+    //await registryUser(this.name.value, this.email.value, this.password.value);
+  }
 };
 
-export default registry;
+export default Registry;
