021f4442d5f4bc0e705b29d6d1d6e10b15d65a0b
Nikolai Makarevich <nikoladze529@mail.ru>
2022-02-05 16:00:19 +0300

    refactor: replace login and registry objects to classes

--- src/components/routes.ts
+++ src/components/routes.ts
@@ -1,15 +1,15 @@
 import home from './home';
 import dictionary from './dictionary';
 import about from './about';
-import registry from './registry';
-import login from './login';
+import Registry from './registry';
+import Login from './login';
 import team from './team';
 import games from './games';
 
 const routes = [
   { path: '/', component: home },
-  { path: 'registry', component: registry },
-  { path: 'login', component: login },
+  { path: 'registry', component: new Registry() },
+  { path: 'login', component: new Login() },
   { path: 'dictionary', component: dictionary },
   { path: 'games', component: games },
   { path: 'about', component: about },
