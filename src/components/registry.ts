2b321edcca1b29051d579c59742ad3d3fe0b25b5
Nikolai Makarevich <nikoladze529@mail.ru>
2022-02-06 21:20:13 +0300

    refactor: add link to login page

--- src/components/registry.ts
+++ src/components/registry.ts
@@ -1,4 +1,4 @@
-import Login from "./login";
+import Login from './login';
 
 class Registry extends Login {
   name: HTMLInputElement;
@@ -13,24 +13,25 @@ class Registry extends Login {
       <form class='main__form'>
         ${this.insertInputs()}
       </form>
+      <a class='login__link' href='#/login'>Вход</a>
     </div>
     `;
-  }
+  };
   override insertInputs(): string {
     super.insertInputs();
     this.name.type = 'text';
     this.name.id = 'name';
     this.name.placeholder = 'Имя пользователя';
-    this.submit.value = 'Зарегистрироваться'
-    
+    this.submit.value = 'Зарегистрироваться';
+
     const div = document.createElement('div');
     div.append(this.name, this.email, this.password, this.submit);
-    return div.innerHTML;    
+    return div.innerHTML;
   }
 
   registry = async () => {
     //await registryUser(this.name.value, this.email.value, this.password.value);
-  }
-};
+  };
+}
 
 export default Registry;
