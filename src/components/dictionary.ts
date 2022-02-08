5c45731e87ea1044265eeb11ef6bd130bfdfa44f
Nikolai Makarevich <nikoladze529@mail.ru>
2022-02-09 00:00:23 +0300

    feat: prepare for add textbook

--- src/components/dictionary.ts
+++ src/components/dictionary.ts
@@ -1,8 +1,17 @@
-const dictionary = {
-  render: (): string => `
+class Dictionary {
+  render = async () => {
+    const mainContainer = document.querySelector('.main .container') as HTMLElement;
+    mainContainer.innerHTML = `
     <div class='main__dictionary'>
       <h2 сlass='main__title'>Словарь</h2>
-    </div>`,
-};
+      <div class="textbook">
+        <div id="textbook-header"></div>
+        <div id="textbook-wrapper"></div>
+      </div>
+    </div>`;
+    textbookNavigation.render();
+    await cards.render();
+  };
+}
 
-export default dictionary;
+export default Dictionary;
