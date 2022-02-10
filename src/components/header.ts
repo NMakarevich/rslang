d97c4a70d8c62f9beed7f45dc41319ea22106821
Nikolai Makarevich <nikoladze529@mail.ru>
2022-02-10 23:56:26 +0300

    refactor: separate header to own class

---
+++ src/components/header.ts
@@ -0,0 +1,74 @@
+class Header {
+  header: HTMLHeadElement;
+
+  constructor() {
+    this.header = document.createElement('header');
+  }
+
+  render = (): void => {
+    const userData = localStorage.getItem('rslang-user') || '';
+    this.header.classList.add('header');
+    this.header.innerHTML = `<div class="container">
+    <div class='header__burger'>
+          <span class='header__burger-line'></span>
+        </div>
+        <nav class='header__nav nav'>
+          <ul class='nav__list'>
+            <li class='nav__item' id='home'><a class='nav__link' href="/">Главная</a></li>
+            <li class='nav__item' id='dictionary'><a class='nav__link' href="#/dictionary">Словарь</a></li>
+            <li class='nav__item' id='games'><a class='nav__link' href="#/games">Игры</a></li>
+            <li class='nav__item' id='about'><a class='nav__link' href="#/about">О приложении</a></li>
+            <li class='nav__item' id='team'><a class='nav__link' href="#/team">Команда</a></li>
+          </ul>
+        </nav>
+        <h1 class='header__title'>RS Lang</h1>
+        <a href='#/login' class='header__link' id='login' ${userData ? 'hidden' : ''}>Войти</a>
+        <div class='header__user'${userData ? '' : 'hidden'}>
+          <span class='header__user-name'>${userData ? JSON.parse(userData)['name'] : ''}</span>
+          <button type='button' class='header__user-button'></button>
+      </div> `;
+    document.body.append(this.header);
+    this.eventListeners();
+  };
+
+  logout = () => {
+    localStorage.setItem('rslang-user', '');
+    this.toggleUser();
+  };
+
+  toggleBurger = () => {
+    if (this.header.classList.contains('open')) {
+      this.header.classList.remove('open');
+      return;
+    }
+    this.header.classList.add('open');
+  };
+
+  toggleUser = (event: CustomEventInit = { detail: '' }) => {
+    const userName = this.header.querySelector('.header__user-name') as HTMLSpanElement;
+    userName.textContent = event.detail;
+    const user = this.header.querySelector('.header__user') as HTMLElement;
+    user.hidden = !user.hidden;
+    const link = this.header.querySelector('.header__link') as HTMLLinkElement;
+    link.hidden = !link.hidden;
+  };
+
+  closeBurger = (event: Event) => {
+    if (!event.target || !event.currentTarget) return;
+    const target = event.target as HTMLElement;
+    if (target.classList.contains('header__burger')) return;
+
+    if (this.header.classList.contains('open')) {
+      this.header.classList.remove('open');
+    }
+  };
+
+  eventListeners() {
+    this.header.querySelector('.header__burger')?.addEventListener('click', this.toggleBurger);
+    document.addEventListener('click', this.closeBurger);
+    document.addEventListener('signin', this.toggleUser);
+    this.header.querySelector('.header__user-button')?.addEventListener('click', this.logout);
+  }
+}
+
+export default Header;
--- src/components/main.ts
+++ src/components/main.ts
@@ -1,72 +1,38 @@
+import Header from './header';
+
+const header = new Header();
+
 class Main {
-  render = (): string => `<header class='header'>
-      <div class="container">
-        <div class='header__burger'>
-          <span class='header__burger-line'></span>
-        </div>
-        <nav class='header__nav nav'>
-          <ul class='nav__list'>
-            <li class='nav__item' id='home'><a class='nav__link' href="/">Главная</a></li>
-            <li class='nav__item' id='dictionary'><a class='nav__link' href="#/dictionary">Словарь</a></li>
-            <li class='nav__item' id='games'><a class='nav__link' href="#/games">Игры</a></li>
-            <li class='nav__item' id='about'><a class='nav__link' href="#/about">О приложении</a></li>
-            <li class='nav__item' id='team'><a class='nav__link' href="#/team">Команда</a></li>
-          </ul>
-        </nav>
-        <h1 class='header__title'>RS Lang</h1>
-        <a href='#/login' class='header__link' id='login'>Войти</a>
-      </div> 
-    </header>
-    <main class='main'>
-      <div class='container'></div>
-    </main>
-    <footer class='footer'>
-      <div class='container'>
-        <a class='footer__school' href="https://rs.school"></a>
-        <nav class='footer__nav nav'>
-          <ul class='nav__list'>
-            <li class='nav__item'><a class='nav__link' href="http://github.com/lisaliza6428" target="_blank">lisaliza6428</a></li>
-            <li class='nav__item'><a class='nav__link' href="http://github.com/nmakarevich" target="_blank">NMakarevich</a></li>
-            <li class='nav__item'><a class='nav__link' href="http://github.com/natakers" target="_blank">natakers</a></li>
-          </ul>
-        </nav>
-        <span class='footer__year'>2022</span>
-      </div>
-    </footer>
+  main: HTMLElement;
+
+  constructor() {
+    this.main = document.createElement('main');
+  }
+
+  render = (): void => {
+    header.render();
+    this.main.classList.add('main');
+    this.main.innerHTML = '<div class="container"></div>';
+    const footer = document.createElement('footer');
+    footer.classList.add('footer');
+    footer.innerHTML = `<div class='container'>
+      <a class='footer__school' href="https://rs.school"></a>
+      <nav class='footer__nav nav'>
+        <ul class='nav__list'>
+          <li class='nav__item'><a class='nav__link' href="http://github.com/lisaliza6428" target="_blank">lisaliza6428</a></li>
+          <li class='nav__item'><a class='nav__link' href="http://github.com/nmakarevich" target="_blank">NMakarevich</a></li>
+          <li class='nav__item'><a class='nav__link' href="http://github.com/natakers" target="_blank">natakers</a></li>
+        </ul>
+      </nav>
+      <span class='footer__year'>2022</span>
+    </div>
     `;
-
-  eventListener() {
-    this.burger.addEventListener('click', this.toggleBurger);
-    document.addEventListener('click', this.closeBurger);
-  }
-
-  get burger(): HTMLElement {
-    return document.querySelector('.header__burger') as HTMLElement;
-  }
+    document.body.append(this.main, footer);
+  };
 
   get mainContainer(): HTMLElement {
-    return document.querySelector('.main .container') as HTMLElement;
+    return this.main.querySelector('.container') as HTMLElement;
   }
-
-  toggleBurger = () => {
-    const header = document.querySelector('.header') as HTMLElement;
-    if (header.classList.contains('open')) {
-      header.classList.remove('open');
-      return;
-    }
-    header.classList.add('open');
-  };
-
-  closeBurger = (event: Event) => {
-    if (!event.target || !event.currentTarget) return;
-    const target = event.target as HTMLElement;
-    if (target.classList.contains('header__burger')) return;
-
-    const header = document.querySelector('.header') as HTMLElement;
-    if (header.classList.contains('open')) {
-      header.classList.remove('open');
-    }
-  };
 }
 
 const main = new Main();
