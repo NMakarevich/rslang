d90cfcd65632e95d4622482538aa8492c127189a
Nikolai Makarevich <nikoladze529@mail.ru>
2022-02-20 00:47:42 +0300

    feat: add consts

--- src/components/consts.ts
+++ src/components/consts.ts
@@ -1,4 +1,41 @@
+import { IStatistics, IUserWord } from './interfaces';
+
 export const baseURL = 'https://rslang-team32.herokuapp.com';
 export const pagesAmount = 30;
 export const chapterDifficult = 6;
 export const answersCount = 5;
+
+export enum Difficulty {
+  'new' = 'new',
+  'hard' = 'hard',
+  'learned' = 'learned',
+}
+
+export const emptyUserWord: IUserWord = {
+  difficulty: Difficulty.new,
+  optional: {
+    answers: '',
+  },
+};
+
+export const emptyUserStatistics: IStatistics = {
+  learnedWords: 0,
+  optional: {
+    sprint: [
+      {
+        date: new Date().toLocaleDateString('ru-RU').split('.').join('-'),
+        wrong: 0,
+        right: 0,
+        rightSequence: 0,
+      },
+    ],
+    audiocall: [
+      {
+        date: new Date().toLocaleDateString('ru-RU').split('.').join('-'),
+        wrong: 0,
+        right: 0,
+        rightSequence: 0,
+      },
+    ],
+  },
+};
